24eb10ca6288738be870a04c3f9efd645835da34
Lauren-Mercedes <lauren.m.smith1310@gmail.com>
2025-06-29 15:51:23 +0100

    Refactor instructions pop up javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,12 @@
 // Instructions Pop Up
-// instructions button
-let instructions = document.getElementById("instructions");
-// popup box
-let popUp = document.getElementById("popup");
-// close popup box button
-let close = document.getElementById("close-popup");
-// click instructions button for popup box
-instructions.addEventListener("click", instructionsPop);
-function instructionsPop(){
-  popUp.style.display="block";
-};
-// click close button to close popup box
-close.addEventListener("click", closePopUp);
-function closePopUp(){
+// When instructions button is clicked show instructions
+function showInstructions(){
+  let popUp = document.getElementById("popup");
+  popUp.style.display = "block";
+}
+//When close button is clicked hide instructions
+function hideInstructions(){
+   let popUp = document.getElementById("popup");
    popUp.style.display = "none";
-};
+}
+
