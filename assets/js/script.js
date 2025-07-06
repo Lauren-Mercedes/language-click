841cee392a3c801f06e4433324a6f444fbb4195f
Lauren-Mercedes <lauren.m.smith1310@gmail.com>
2025-07-06 16:06:29 +0100

    Add js object class to define the question structure for adding questions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,26 @@
+/**
+ * Class that provides the blueprint of the question structure for use in adding questions.
+ * Includes the question, the correct answer, and answer options.
+ * Includes functions that allow the question, correct answer, and answer options to be called for use in other functions.
+ */
+class questionStructure{
+// question structure
+  constructor (question, correct, answers){
+    this.question = question;
+    this.correct = correct;
+    this.answerSet = answers;
+  }
+// functions 
+  getQuestion = () =>{
+    return this.question;}
+
+    getCorrect = () =>{
+    return this.correct;}
+
+    getAnswers = () =>{
+    return this.answerSet;}
+}
+
 // Instructions Pop Up
 // When instructions button is clicked show instructions
 function showInstructions(){
